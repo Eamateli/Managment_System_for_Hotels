c3c37ad0bcbf4331892cd483b4f1f467ad04640c
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-13 21:17:06 +0200

    Adding some more elements in the html file

--- base/views.py
+++ base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 class TaskList(ListView):
     
-    model = Task 
+    model = Task
+    context_object_name = 'tasks'
+    
+class TaskDetails(DetailView):
+    model = Task
 
