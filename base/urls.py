6e9141726c17a85307f4937dc3593f528895c1b5
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-23 22:26:51 +0200

    Adding and eddit option next to the view. Importing UpdateView,next to CreateView, Creating the class TaskUpdate,adding the path, adn adding the url on the task_list.html

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from .views import TaskList, TaskDetails, TaskCrete
+from .views import TaskList, TaskDetails, TaskCrete, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetails.as_view(), name='tasks'),
     path('task-create/', TaskCrete.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
 
 ]
