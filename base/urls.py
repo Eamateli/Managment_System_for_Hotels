cf7e5ecf52abdc462e33115008f45dc3af3ad44e
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-24 18:41:24 +0200

    Adding a delete item. This option allows to delete and entry. created a new task_confirm_delete.html tempalte wiht a form and a back link, added the DeleteView and class, added the url and lastlly the delete option via the task_list.html template

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path 
-from .views import TaskList, TaskDetails, TaskCrete, TaskUpdate
+from .views import TaskList, TaskDetails, TaskCrete, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetails.as_view(), name='tasks'),
     path('task-create/', TaskCrete.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+    
+    
 
 ]
