e5fcf08a4247269b369bd2c13e94e6b64002b5a0
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-26 16:17:24 +0200

    adding the apps addon, hotel, user_dashbaord, userauths to the settings.py

--- Mng_Sys/settings.py
+++ Mng_Sys/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'addon',
+    'hotel',
+    'user_dashboard',
+    'userauths',
+    
 ]
 
 MIDDLEWARE = [
