bbc435a02031559970107b4517b30be35f66627e
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-28 22:47:35 +0200

    Created the User class, Created the Profile class

--- Mng_Sys/settings.py
+++ Mng_Sys/settings.py
@@ -150,6 +150,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTh_USER_MODEL = "userauth.User"
+
 
 JAZZMIN_SETTINGS = {
     'site_header': "Luxury",
