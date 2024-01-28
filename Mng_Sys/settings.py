94f97304ce62af288ef1acf33327cfbc06626afd
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-28 16:50:15 +0200

    Adding a new logo

--- Mng_Sys/settings.py
+++ Mng_Sys/settings.py
@@ -132,9 +132,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -145,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 JAZZMIN_SETTINGS = {
     'site_header': "Luxury",
     'site_brand': "Luxury",
-    'site_logo': "static/images/logo.png",
+    'site_logo': "images/logo.png",
     'copyright': "The Agency",
     'welcome_sign':"Welcome to Luxury, Admin! Please Log in.",
     'topmenu_links': [
@@ -193,7 +202,6 @@ JAZZMIN_SETTINGS = {
 }
 
 JAZZMIN_UI_TWEAKS = {
-    
     "navbar_small_text": False,
     "footer_small_text": False,
     "body_small_text": True,
