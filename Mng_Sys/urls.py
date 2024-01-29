764f3289e48e621c425e58618e1916d97f12597a
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-29 23:36:38 +0200

    Updating file

--- Mng_Sys/urls.py
+++ Mng_Sys/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("user/", include("userauths.urls"))
+    path("user/", include("userauths.urls")),
+    path("", include("hotel.urls")),
     
 ]
