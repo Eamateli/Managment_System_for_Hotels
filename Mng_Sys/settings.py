97ff0cba5002486ac5be8929d8ebaaf431b1b56c
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-29 13:28:21 +0200

    removing users and user_permissions from the class User in userauths/models.py

--- Mng_Sys/settings.py
+++ Mng_Sys/settings.py
@@ -150,7 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTh_USER_MODEL = "userauths.User"
+AUTH_USER_MODEL = "userauths.User"
 
 
 JAZZMIN_SETTINGS = {
