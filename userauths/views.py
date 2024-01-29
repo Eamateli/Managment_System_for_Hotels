98b4e1724ee80b09acd5cae78f200a7306b8f216
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-01-29 18:53:57 +0200

    Added at templates/userauths/sign-up.html csrf_token,form.as_p, form tag.Added a urls.py at userauths and a path. Worked on views.py for userauths.

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from userauths.models import User, Profile
+from userauths.forms import UserRegisterForm
 
-# Create your views here.
+def RegisterView(request):
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        full_name = form.clean_password2.get("full_name")
+        
+        
+        
+        
+        
+    context = {
+        "form":form
+    }
+    return render(request,"userauths/sign-up.html", context)
+    
+    
